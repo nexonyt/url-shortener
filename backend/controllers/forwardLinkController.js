de01e7d39dc8e1d1af89dd3c8689e76ef0089722
Marcin Kaczmarek <kaczmarekm090@gmail.com>
2025-03-11 23:14:48 +0100

    Added collecting data during redirect and changes in documentation

--- backend/controllers/forwardLinkController.js
+++ backend/controllers/forwardLinkController.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql');
 const jwt = require('jsonwebtoken');
 const path = require('path');
-const { get } = require('http');
+const axios = require('axios')
 const { logger } = require('../helpers/logger');
 const dotenv = require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+const UAParser = require('ua-parser-js');
+
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -29,13 +31,12 @@ const decrementLinkUsage = async (shortLinkId) => {
 };
 
 
-const addTrackingToDB = async (req, res) => {
+const addTrackingToDB = async (linkData, collectedUserInfo, status) => {
   console.log(`Dodanie rekordu`);
 
-  const SQL = `INSERT INTO links_tracking (link_id,user_agent) VALUES ('1','polibuda')`;
+  const SQL = `INSERT INTO links_tracking (link_id,user_agent,user_ip,isp,country,city,accept_language,timezone,sucess_redirect,referer,os,browser,created_at,cpu,device_type) VALUES ('${linkData.id}','${collectedUserInfo.userAgent}','${collectedUserInfo.ip}','${collectedUserInfo.geo.isp}','${collectedUserInfo.geo.country}','${collectedUserInfo.geo.city}','${collectedUserInfo.acceptLanguage}','${collectedUserInfo.geo.timezone}',${status},'${collectedUserInfo.referer}','${collectedUserInfo.device.os + ' ' + collectedUserInfo.device.osVersion}','${collectedUserInfo.device.browser + ' ' + collectedUserInfo.device.browserVersion}',NOW(),'${collectedUserInfo.device.cpu}','${collectedUserInfo.device.deviceType}')`;
 
   db.query(SQL, async (err, result) => {
-    console.log(result)
     console.log(err)
   });
 
@@ -44,27 +45,69 @@ const addTrackingToDB = async (req, res) => {
 
 const forwardLink = async (req, res) => {
 
+  async function getGeoData(ip) {
+    let geoData = {};
+    try {
+      const response = await axios.get(`http://ip-api.com/json/${ip}?fields=status,country,city,timezone,isp,proxy,zip,lat,lon,continent`);
+      if (response.data.status === "success") {
+        geoData = {
+          country: response.data.country || "N/A",
+          city: response.data.city,
+          timezone: response.data.timezone,
+          isp: response.data.isp,
+          isVpn: response.data.proxy,
+          zip: response.data.zip,
+          lat: response.data.lat,
+          lon: response.data.lon,
+          continent: response.data.continent
+        };
+      }
+    } catch (error) {
+      logger("Błąd pobierania geolokalizacji:", error.message);
+    }
 
-  const userInfo = {
-    // ip: getClientIP(req),
+    return geoData;
+  }
+
+  const getDeviceType = (userAgent) => {
+    const ua = userAgent.toLowerCase();
+    if (/mobile|android|iphone|ipad|ipod/i.test(ua)) return "mobile";
+    return "desktop";
+};
+
+  const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+  const geoData = await getGeoData(clientIp);
+  const userAgent = req.headers["user-agent"];
+  const parser = new UAParser(userAgent);
+  const deviceInfo = parser.getResult();
+  const collectedUserInfo = {
+    ip: clientIp,
     userAgent: req.headers["user-agent"],
     referer: req.headers["referer"] || "N/A",
     acceptLanguage: req.headers["accept-language"] || "N/A",
     encoding: req.headers["accept-encoding"] || "N/A",
     cookies: req.headers["cookie"] || "N/A",
     origin: req.headers["origin"] || "N/A",
-    // geo: {
-    //     country: geoData.country || "Unknown",
-    //     city: geoData.city || "Unknown",
-    //     timezone: geoData.timezone || "Unknown",
-    //     isp: geoData.isp || "Unknown",
-    //     isVpn: geoData.proxy || false,
-    // },
-    browserInfo: req.body.browserInfo || {},
+    geo: {
+      country: geoData.country || "N/A",
+      city: geoData.city || "N/A",
+      timezone: geoData.timezone || "N/A",
+      isp: geoData.isp || "N/A",
+      isVpn: geoData.proxy || false,
+    },
+    device: {
+      browser: deviceInfo.browser.name || "N/A",
+      browserVersion: deviceInfo.browser.version || "N/A",
+      os: deviceInfo.os.name || "N/A",
+      osVersion: deviceInfo.os.version || "N/A",
+      deviceType: getDeviceType(userAgent) || "N/A",
+      deviceVendor: deviceInfo.device.vendor || "N/A",
+      cpu: deviceInfo.cpu.architecture || "N/A",
+    }
   };
 
-  console.log("Dane użytkownika:", userInfo);
-
+  console.log(collectedUserInfo)
 
   logger('Asking DB for redirection link for: ' + req.params.id);
   const SQL = `SELECT * FROM links WHERE short_link = "https://nexonstudio.pl/${req.params.id}"`;
@@ -82,7 +125,7 @@ const forwardLink = async (req, res) => {
             logger(`Link disabled: ${req.params.id}`);
             return res.redirect(302, process.env.APP_INACTIVE_URL);
           } else {
-            await addTrackingToDB();
+            await addTrackingToDB(result[0], collectedUserInfo, 1);
             // if czy jest limit i czy nalezy wykonać dekrementacje + jezeli tak to czy jest wiekszy od 0
             if (result[0].usage_limit && result[0].usage_limit <= 0) {
               logger(`Link usage limit reached for requested link: ${req.params.id}`);
